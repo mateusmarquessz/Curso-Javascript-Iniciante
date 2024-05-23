8c9fff24822ca4414a98737cb369dc7d621fd50f
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-05-23 13:33:11 -0300

    estudos

---
+++ Aulas/08-Tipos de Funcao/index.js
@@ -0,0 +1,19 @@
+//tipos de funcoes
+//existem 2 tipos
+//Funcao que realiza uma tarefa e nao devolve nada
+function dizerNome(){
+    console.log("Mateus");
+}
+//nao ouve retorno
+dizerNome();
+
+
+
+//Eu posso usar a funcao na variavel, usando o return
+function Multiplicar(Valor){
+    return valor * 2;
+}
+
+console.log(Multiplicar(5));
+
+let resultado = Multiplicar(5);
---
+++ Aulas/10-Operadores Aritimeticos/index.js
@@ -0,0 +1,17 @@
+
+let salario = 100;
+
+
+console.log(salario + salario);
+console.log(salario - salario);
+console.log(salario * salario);
+console.log(salario / salario);
+console.log(5 ** 5);
+
+// ++ Operadores de inclemento 
+let  idade = 18;
+
+console.log(idade++);
+
+console.log(idade);
+console.log(idade--)
